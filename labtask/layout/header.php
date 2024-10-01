a8b40dd6a4b519351ae6d8ddbe02cf7641d87e5d
Jobaer-Hossain <tuhinjh027@gmail.com>
2024-10-01 09:18:51 +0600

    Add files via upload

---
+++ labtask/layout/header.php
@@ -0,0 +1,17 @@
+<html>
+
+<body>
+
+<div class="sticky">
+    <table>
+        <tr>
+            <td><a href="">Home</a></td>
+            <td><a href="">About Us</a></td>
+            <td><a href="">Services</a></td>
+            <td><a href="">Contuct Us</a></td>
+            <td><a href="">Login</a></td>
+        </tr>
+</table>
+</div>
+</body>
+</html>
